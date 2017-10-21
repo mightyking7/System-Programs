442bbf43a9e443a40684de92acea3d3ec9c9065d
Isaac Buitrago <mightyking7@gmail.com>
2017-10-20 22:18:24 -0500

    added -i switch support for returning files that do not match the passed in pattern in name and contents

--- find.pl
+++ find.pl
@@ -1,17 +1,15 @@
 #!/usr/bin/perl -w
 
 
-my $option = 0;     #run the script in inverted order
+my $ioption = 0;     #run the script in inverted order
 my $regex;          #regular expression to find in file names and contents
 my @files;          #fileList passed to the script
 
 die "Usage: find.pl [-i] pattern fileList" if($#ARGV < 1);
 
-#initialize variables and determine the program state
-
 # determine of the -i option is given
 if($ARGV[0] eq "-i" ){
-    $option = 1;
+    $ioption = 1;
     $regex = $ARGV[1];
 }
 
@@ -23,51 +21,71 @@ else{
 @files = grep( -f, @ARGV);
 
 #run the script in inverted mode
-=pod
-if($option){
 
-      foreach my $file(@files){
+if($ioption){
 
+      foreach (@files){
+        
+        #extract the file name from the file path
+        ($file) = $_ =~ /.*\/(.*)/g;
+
+        #if the file name does not match the pattern, open it and ensure that it's
+        #contents do not also match the pattern
         if($file !~ m/$regex/){
+
+            $match = 0;
+
+            open(IN, "<", $_);
+
+            while($line = <IN>){
+
+                if($line =~ m/$regex/){
+                    $match =1;
+                }
+            }
+
+            if(not $match){
+                push(@invertedFiles, $file);
+            }
+
+            close(IN);
+        }       
+    }
+
+    print (join("\n", @invertedFiles), "\n");
+}
+
+
+else{
+
+    foreach (@files){
+
+        ($file) = $_ =~ /.*\/(.*)/g;
+
+        #Print the name of the file if it matches the pattern,
+        #else check if the file contents match a pattern.
+        if($file =~ m/$regex/){
             print "$file\n";
         }
 
         else{
 
-            open(IN, "<", $file);
-
-            while( ($line = <IN>) && ($line !~ /$regex/);
-
-            $? !=
-            }
-            close(IN);
-        }
-    }
-}
-=cut
-
-    foreach my $file(@files){
-
-        if($file =~ m/$regex/){
-            #match the '/' to print only the file name
-            print "$file";
-        }
-
-        else{
-
-            open(IN, "<", $file);
+            open(IN, "<", $_) or die("Failed to open $_");
 
             while( $line = <IN>){
 
                 if($line =~ /$regex/){
 
-                    print "$file: $line\n";
+                    print "$file: $line";
                     last;
                 }
             }
             close(IN);
         }
     }
+}
+
+
 
 
 
